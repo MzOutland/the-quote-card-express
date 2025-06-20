fead1eccde47b1921b5e0b05044158b1dea17ca5
Angela Outland <angela.outland@gmail.com>
2025-06-20 11:06:17 -0400

    add background images from unsplash

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,29 @@ const elements = {
     author: document.getElementById("author"),
 };
 
-const quotes = [
+async function getRandomImage() {
+    const client_id = "CrsabaiYUQwpJhFkeYLKRjZMWN_Op46hSIxWSXQHNNk";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        console.log(receivedPhotoUrl);  // 👈 This checks if the image URL is valid
+
+
+        const imgDiv = document.querySelector(".background-img");
+        console.log(imgDiv);
+        
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getRandomImage();
+
+/* const quotes = [
     {
         quote: "Rejoice Always", 
         author: "1 Thessalonians 5:16", 
@@ -40,4 +62,4 @@ function loopThroughQuotes() {
     }, 3000);
 
 }
-setTimeout(loopThroughQuotes, 3000);
+setTimeout(loopThroughQuotes, 3000); */
